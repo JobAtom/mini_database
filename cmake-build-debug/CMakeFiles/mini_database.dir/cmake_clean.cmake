32dfd22d43d4a49718ab45ca445ce1bce1d21c03
AJ <jinaobo-cid@163.com>
2018-03-20 17:46:29 -0500

    mini_databeas project begin
    Xiaoxinxin and me start to coding.

---
+++ cmake-build-debug/CMakeFiles/mini_database.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mini_database.dir/parser/bison_parser.cpp.o"
+  "CMakeFiles/mini_database.dir/parser/flex_lexer.cpp.o"
+  "CMakeFiles/mini_database.dir/sql/Expr.cpp.o"
+  "CMakeFiles/mini_database.dir/sql/statements.cpp.o"
+  "CMakeFiles/mini_database.dir/main.cpp.o"
+  "CMakeFiles/mini_database.dir/sqlhelper.cpp.o"
+  "CMakeFiles/mini_database.dir/SQLParser.cpp.o"
+  "CMakeFiles/mini_database.dir/SQLParserResult.cpp.o"
+  "mini_database.pdb"
+  "mini_database"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mini_database.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,56 +1,13 @@
+//
+// Created by JinAobo on 3/20/18.
+//
+
 #include <iostream>
-#include <stdlib.h>
 #include <string>
 
-// include the sql parser
-#include "SQLParser.h"
-
-// contains printing utilities
-#include "sqlhelper.h"
-#include <cassert>
-#include <vector>
-#include <fstream>
-#include <sstream>
-#include "dbms.h"
-//xiaoxinxin zai zhe le 
-
 using namespace std;
-using namespace dbms;
 
-int main(int argc, char *argv[])
-{
-    if (argc <= 1) {
-        fprintf(stderr, "Usage:\n./dbms script=filename\n\tOR\n./dbms \"SQL query\"\n");
-        return -1;
-    }
-
-    std::string param = argv[1];
-    DBMS db;
-    if(param.find("script=") == 0){
-        db.setMode(RUN_MODE::SCRIPT);
-
-        std::ifstream file(param.substr(7));
-        std::stringstream buffer;
-        buffer << file.rdbuf();
-        db.processQuery(buffer.str());
-    }else{
-
-        string query = param;
-
-        db.setMode(RUN_MODE::INTERACTIVE);
-
-        while(true){
-            if(db.processQuery(query))
-                return 0;
-
-            cout<<"SQL > ";
-            query.clear();
-            do{
-                string line;
-                getline(cin, line);
-                query += " " + line;
-            }while (query.find(";") == string::npos);
-
-        }
-    }
+int main(int argc, char * argv[]){
+    cout<<"xiaoxinxin ni hao "<<endl;
+    cout<<"begin mini_database project!!!"<<endl;
 }
