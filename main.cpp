fb429bec9822f336af0b237f96bf5591e84d92af
AJ <jinaobo-cid@163.com>
2018-03-20 18:38:10 -0500

    main function
    Use sqlparser.
    
    Co-Authored-By: Xin <xzhou29@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -4,10 +4,50 @@
 
 #include <iostream>
 #include <string>
+#include "SQLParser.h"
+#include "sqlhelper.h"
 
 using namespace std;
 
 int main(int argc, char * argv[]){
-    cout<<"xiaoxinxin ni hao "<<endl;
     cout<<"begin mini_database project!!!"<<endl;
+
+    if(argc <= 1){
+        cout<<"please input query to start SQL!"<<endl;
+        exit(1);
+    }
+    string query = "";
+    for(int i = 1 ; i < argc; i ++ ){
+        query += argv[i];
+        query += " ";
+    }
+
+    while(true) {
+        hsql::SQLParserResult *result = hsql::SQLParser::parseSQLString(query);
+
+        // check whether the parsing was successful
+        if (result->isValid()) {
+            for (unsigned i = 0; i < result->size(); ++i) {
+                //run sql query
+                cout<< 'run sql query'<<endl;
+                
+            }
+        } else {
+            cout << "Given string is not a valid SQL query." << endl
+                 << result->errorMsg() << "(" << result->errorLine() << ":" << result->errorColumn() << ")" << endl;
+        }
+        delete result;
+        query.clear();
+        cout << "SQL>";
+        while(query.find(";") == string::npos){
+            string line;
+            getline(cin, line);
+            query += line;
+        }
+        if (query == "quit;")
+            return 0;
+
+    }
+    return 0;
+
 }
